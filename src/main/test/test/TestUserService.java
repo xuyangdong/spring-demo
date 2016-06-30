4a3bdbed3dfd5f3cafc96735ec2d4eeacf70e1b0
xuyangdong <804954666@qq.com>
2016-06-30 23:39:29 +0800

    添加测试

---
+++ src/main/test/test/TestUserService.java
@@ -0,0 +1,23 @@
+package test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import service.UserService;
+
+/**
+ * Created by xyd on 16/6/30.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)//基于junit4的spring测试框架
+@ContextConfiguration(locations = "/applicationContext.xml")//启动spring容器
+public class TestUserService {
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void hasMatchUser(){
+
+    }
+}
